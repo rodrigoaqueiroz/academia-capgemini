e3d831295960bcc32fc84fb58dccd0cc83ccf775
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-02-17 19:09:30 -0300

    test: req03 implementation

---
+++ tests/q03.test.js
@@ -0,0 +1,28 @@
+const { countAnagrams } = require('../src/requirements/q03');
+
+describe('test countAnagrams function', () => {
+  let stdin;
+  beforeEach(() => {
+    stdin = require('mock-stdin').stdin();
+  });
+
+  it('test if ovo returns 2', async () => {
+    let INPUT = 'ovo'
+    process.nextTick(() => {
+      stdin.send(INPUT);
+    });
+    const result = await countAnagrams(INPUT);
+    expect(result).toStrictEqual(2);
+    stdin.restore();
+  }); 
+
+  it('test if ifailuhkqq returns 3', async () => {
+    let INPUT = 'ifailuhkqq'
+    process.nextTick(() => {
+      stdin.send(INPUT);
+    });
+    const result = await countAnagrams(INPUT);
+    expect(result).toStrictEqual(3);
+    stdin.restore();
+  }); 
+});
